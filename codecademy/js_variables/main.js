9bbb0c0cc097269e6914b23d8599d74b466905af
Marlene Benzinger <marlene.benzinger@student.uibk.ac.at>
2021-03-25 13:54:16 +0100

    The Increment and Decrement Operator

--- codecademy/js_variables/main.js
+++ codecademy/js_variables/main.js
@@ -1,21 +1,4 @@
-let levelUp = 10;
-let powerLevel = 9001;
-let multiplyMe = 32;
-let quarterMe = 1152;
-levelUp += 5;
-powerLevel-=100;
-multiplyMe *=11;
-quarterMe /=4;
-// Use the mathematical assignments in the space below:
-
-
-
-
-
-
-// These console.log() statements below will help you check the values of the variables.
-// You do not need to edit these statements. 
-console.log('The value of levelUp:', levelUp); 
-console.log('The value of powerLevel:', powerLevel); 
-console.log('The value of multiplyMe:', multiplyMe); 
-console.log('The value of quarterMe:', quarterMe);
+let gainedDollar = 3;
+let lostDollar = 50;
+gainedDollar++;
+lostDollar--;
