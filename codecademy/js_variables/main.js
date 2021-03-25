fc324000e3df5ebff4e482ccafc4fb1687f5bea3
Marlene Benzinger <marlene.benzinger@student.uibk.ac.at>
2021-03-25 13:48:26 +0100

    Create a Variable: let

--- codecademy/js_variables/main.js
+++ codecademy/js_variables/main.js
@@ -1,6 +1,5 @@
-var favoriteFood = 'pizza';
-console.log(favoriteFood);
-var numOfSlices = 8;
-console.log (numOfSlices);
-console.log(favoriteFood);
-console.log(numOfSlices);
+let changeMe = true;
+
+changeMe = false;
+
+console.log(changeMe);
