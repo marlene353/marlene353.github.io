c2ab54a106c43d6a9f6352ebe6857af12a55c307
Marlene Benzinger <marlene.benzinger@student.uibk.ac.at>
2021-03-25 17:25:20 +0100

    Route Und Karte

--- nz/main.js
+++ nz/main.js
@@ -20,11 +20,14 @@ const map = L.map("map", {
     ]
   });
   
-  let mrk = L.marker([ -45.09746, 170.96911 ]).addTo(map);
-  mrk.bindPopup(`
-  <h4>Stop ${stop.5}: ${stop.Oamaru}<h4>
-  <p><a href="${stop.wikipedia}"><i class="fas fa-external-link-alt mr-3"></i>Read about 
-  stop in Wikipedia</a></p>
+  console.log(ROUTE);
+for (let entry of ROUTE) {
+  console.log(entry);
+  let mrk = L.marker([entry.lat, entry.lng]).addTo(map);
+  mrk.bindPopup(`<h4>Stop ${entry.nr}: ${entry.name}<h4>
+<p><a href="${entry.wikipedia}"><i class="fas fa-external-link-alt mr-3"></i>Read about stop in Wikipedia</a></p>
 `).openPopup();
 
-//console.log(document.querySelector("#map"));
+}
+
+console.log(document.querySelector("#map"));
