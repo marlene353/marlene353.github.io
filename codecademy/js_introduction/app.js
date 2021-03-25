321a1e5ae769cfa43cf371fb4ea146828c4a0f38
Marlene Benzinger <marlene.benzinger@student.uibk.ac.at>
2021-03-25 12:13:23 +0100

    Built-In Objects

--- codecademy/js_introduction/app.js
+++ codecademy/js_introduction/app.js
@@ -1,5 +1,3 @@
-// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
-console.log('Codecademy'.toUpperCase());
-
-// Use a string method to log the following string without whitespace at the beginning and end of it.
-console.log('    Remove whitespace   '.trim());
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
