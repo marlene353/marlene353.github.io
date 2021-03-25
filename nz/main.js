af1255a3255b7225163ccb5af72b30ffb9ec9899
Marlene Benzinger <marlene.benzinger@student.uibk.ac.at>
2021-03-25 16:21:34 +0100

    Marker

--- nz/main.js
+++ nz/main.js
@@ -1,5 +1,12 @@
-console.log("Hello World");
-console.log(L);
+const map = L.map("map", {
+    center: [ -39.29, 175.56  ],
+    zoom: 13,
+    layers: [
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+    ]
+  });
+  
+  let mrk = L.marker([ -39.29, 175.56 ]).addTo(map);
 
-const map = L.map("map");
-console.log(document.querySelector("map"))
+
+  console.log(document.querySelector("#map"));
