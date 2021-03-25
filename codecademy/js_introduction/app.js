d54ca0e8f4fc83c20c6a42f9a5cefab6f5e9cf27
Marlene Benzinger <marlene.benzinger@student.uibk.ac.at>
2021-03-25 12:09:57 +0100

    Methods

--- codecademy/js_introduction/app.js
+++ codecademy/js_introduction/app.js
@@ -1,1 +1,5 @@
-console.log ('Teaching the world how to code'.length);
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
