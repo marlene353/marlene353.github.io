21a1016972a6953eb5ba9e0b006b8ec624496d5e
Marlene Benzinger <marlene.benzinger@student.uibk.ac.at>
2021-03-25 12:02:40 +0100

    Arithmetic Operators

--- codecademy/js_introduction/app.js
+++ codecademy/js_introduction/app.js
@@ -1,4 +1,4 @@
-console.log('JavaScript');
-console.log(2011);
-console.log('Woohoo! I love to code! #codecademy');
-console.log(20.49);
+console.log(10 + 3.5)
+console.log(2019 - 1969);
+console.log(65 / 240);
+console.log(0.2708 * 100);
