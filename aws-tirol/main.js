66499b338d9babf407f9695f9d87d1c3c4da2245
Marlene Benzinger <marlene.benzinger@student.uibk.ac.at>
2021-04-15 17:54:51 +0200

    next step

---
+++ aws-tirol/main.js
@@ -0,0 +1,56 @@
+
+let basemapGray = L.tileLayer.provider('BasemapAT.grau');
+
+let map = L.map("map", {
+    center: [47, 11],
+    zoom: 9,
+    layers: [ 
+        basemapGray
+    ]
+});
+
+let layerControl = L.control.layers({
+    "BasemapAT.grau": basemapGray,
+    "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto'),
+    "BasemapAT.surface": L.tileLayer.provider('BasemapAT.surface'),
+    "BasemapAT.overlay+ortho": L.layerGroup([
+        L.tileLayer.provider('BasemapAT.orthofoto'),
+        L.tileLayer.provider('BasemapAT.overlay')
+    ])
+}).addTo(map);
+
+
+let awsUrl = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
+
+let awsLayer = L.featureGroup();
+layerControl.addOverlay(awsLayer, "Wetterstationen Tirol");
+awsLayer.addTo(map);
+
+fetch(awsUrl)
+    .then(response => response.json())
+    .then(json => {
+        console.log('Daten konvertiert: ', json);
+        for (station of json.features) {
+            // console.log('Station: ', station);
+            let marker = L.marker([
+                station.geometry.coordinates[1],
+                station.geometry.coordinates[0]
+            ]);
+            let formattedDate = new Date(station.properties.date);
+            marker.bindPopup(`
+            <h3>${station.properties.name}</h3>
+            <ul>
+              <li>Datum: ${formattedDate.toLocaleString("de")}</li>
+              <li>Seehöhe: ${station.geometry.coordinates[2]} m</li>
+              <li>Temperatur: ${station.properties.LT} C</li>
+              <li>Schneehöhe: ${station.properties.HS || '?'} cm</li>
+              <li>Windgeschwindigkeit: ${station.properties.WG || '?'} km/h</li>
+              <li>Windgeschwindrichtung: ${station.properties.WR || '?'}</li>
+            </ul>
+            <a target="_blank" href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.plot}.png">Grafik</a>
+            `);
+            marker.addTo(awsLayer);
+        }
+        // set map view to all stations
+        map.fitBounds(awsLayer.getBounds());
+    });
