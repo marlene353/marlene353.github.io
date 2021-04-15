99f4e345762418a94dd8ff2e83a9b6f3a04ef380
Marlene Benzinger <marlene.benzinger@student.uibk.ac.at>
2021-04-15 10:26:42 +0200

    fehler

--- nz/main.js
+++ nz/main.js
@@ -7,9 +7,11 @@ let stop = {
     wikipedia: "https://de.wikipedia.org/wiki/Oamaru"
 };
 
+console.log(stop.name);
+
 const map = L.map("map", {
-    center: [ stop.lat, stop.lng  ],
-    zoom: 13,
+   // center: [ stop.lat, stop.lng  ],
+    // zoom: 13,
     layers: [
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
     ]
@@ -21,18 +23,16 @@ const map = L.map("map", {
 
   //console.log(ROUTE);
   ROUTE.sort ((stop1,stop2)=> {
-      return stop1nr. > stop2.nr
+      return stop1.nr > stop2.nr
   });
 
  for (let entry of ROUTE) {
   //console.log(entry);
 
-  nav.innerHTML +=   nav.innerHTML += `
+  nav.innerHTML += `
   <option value="${entry.user}">Stop ${entry.nr}: ${entry.name}</option>
   `;
   
-
-
   let mrk = L.marker([entry.lat, entry.lng]).addTo(map);
   mrk.bindPopup(`
    <h4>Stop ${entry.nr}: ${entry.name}<h4>
@@ -40,12 +40,22 @@ const map = L.map("map", {
    about stop in Wikipedia</a></p>
 `);
 
-if(entry.nr=5) {
-    map.setView([entry.lat, entry.lng], 13)  
+if(entry.nr==5) {
+    map.setView([entry.lat, entry.lng], 5)  
     mrk.openPopup ();
     }
 }
 
+nav.onchange = (evt) => {
+    console.log(evt.target.selectedIndex);
+    let selected = evt.target.selectedIndex;
+    let options = evt.target.options;
+    let username = options[selected].value;
+    let link = `https://${username}.github.io/nz/index.html`;
+    console.log(username, link);
+
+    window.location.href = link;
+};
 
 //<option value="webmapping">Oamaru</option>
 //console.log(document.querySelector("#map"));
