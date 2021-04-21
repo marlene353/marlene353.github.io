734a09b554e7ff96a4c1b6801bfe6f94f3ba1c17
Marlene Benzinger <marlene.benzinger@student.uibk.ac.at>
2021-04-21 17:38:26 +0200

    hinzugefügt aber noch nicht in html eingebunden

---
+++ aws-tirol/colors.js
@@ -0,0 +1,37 @@
+const COLORS = {
+    windspeed: [{
+        min: 0,
+        max: 5,
+        col: "#ffff64"
+    }, {
+        min: 5,
+        max: 10,
+        col: "#c8ff64"
+    }, {
+        min: 10,
+        max: 20,
+        col: "#96ff96"
+    }, {
+        min: 20,
+        max: 40,
+        col: "#32c8ff"
+    }, {
+        min: 40,
+        max: 60,
+        col: "#6496ff"
+    }, {
+        min: 60,
+        max: 80,
+        col: "#9664ff"
+    }, {
+        min: 80,
+        max: 999,
+        col: "#ff3232"
+    }],
+    temperature: [
+
+    ],
+    snowheight: [
+
+    ]
+};
