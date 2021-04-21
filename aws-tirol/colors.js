5f5d9df2395dfccabc723d35203655ef1799f9b1
Marlene Benzinger <marlene.benzinger@student.uibk.ac.at>
2021-04-21 18:49:33 +0200

    Temp & Snowheight ergänzt

--- aws-tirol/colors.js
+++ aws-tirol/colors.js
@@ -28,10 +28,90 @@ const COLORS = {
         max: 999,
         col: "#ff3232"
     }],
-    temperature: [
-
-    ],
-    snowheight: [
-
-    ]
+    temperature: [{
+        min: -50,
+        max: -25,
+        col: "#9f80ff"
+    }, {
+        min: -24,
+        max: -20,
+        col: "#784cff"
+    }, {
+        min: -19,
+        max: -15,
+        col: "#0f5abe"
+    }, {
+        min: -14,
+        max: -10,
+        col: "#1380ff"
+    }, {
+        min: -9,
+        max: -5,
+        col: "#19cdff"
+    }, {
+        min: -4,
+        max: 0,
+        col: "#8fffff"
+    }, {
+        min: 0,
+        max: 5,
+        col: "#b0ffbc"
+    }, {
+        min: 6,
+        max: 10,
+        col: "#ffff73"
+    }, {
+        min: 11,
+        max: 15,
+        col: "#ffbe7d"
+    }, {
+        min: 16,
+        max: 20,
+        col: "#ff9b41"
+    }, {
+        min: 21,
+        max: 25,
+        col: "#ff1e23"
+    }, {
+        min: 26,
+        max: 30,
+        col: "#ffff64"
+    }, {
+        min: 31,
+        max: 100,
+        col: "#ffff64"
+    }],
+    snowheight: [{
+        min: 0,
+        max: 10,
+        col: "#ffffb2"
+    }, {
+        min: 11,
+        max: 25,
+        col: "#b0ffbc"
+    }, {
+        min: 26,
+        max: 50,
+        col: "#8cffff"
+    }, {
+        min: 51,
+        max: 100,
+        col: "#19cdff"
+    }, {
+        min: 101,
+        max: 200,
+        col: "#1982ff"
+    }, {
+        min: 201,
+        max: 300,
+        col: "#1982ff"
+    }, {
+        min: 301,
+        max: 400,
+        col: "#784bff"
+    }, {
+        min: 401,
+        max: 600,
+        col: "#cd0feb"
+    }]
 };
