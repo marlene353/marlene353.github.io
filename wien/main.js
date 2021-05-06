53c8b9517366a8d1e13f245a60691b713bd1136c
Marlene Benzinger <marlene.benzinger@student.uibk.ac.at>
2021-05-06 18:14:21 +0200

    farbe für styleIntervals

--- wien/main.js
+++ wien/main.js
@@ -170,7 +170,22 @@ var miniMap = new L.Control.MiniMap(
 //Reachability plugin
 
 let styleIntervals = (feature) => {
-    console.log(feature.properties);
+    //console.log(feature.properties);
+    //console.log(feature.properties.Measure)
+    let color= "";
+    let range = feature.properties.Range;
+    if (feature.properties.Measure === "time") {
+        color = COLORS.minutes[range];
+    } else if (feature.properties.Measure === "distance") {
+       color = COLORS.kilometers[range];
+    } else {
+        color = "black";
+    }
+    return {
+        color: color,
+        opacity: 0.5,
+        fillOpacity: 0.2
+    };
 };
 
 L.control.reachability({
