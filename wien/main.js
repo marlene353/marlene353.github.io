a89eae2f2fcf463fe2e0989157365151ce78cb18
Marlene Benzinger <marlene.benzinger@student.uibk.ac.at>
2021-05-06 17:43:01 +0200

    font awesome icons

--- wien/main.js
+++ wien/main.js
@@ -170,5 +170,21 @@ var miniMap = new L.Control.MiniMap(
     //Reachability plugin
     L.control.reachability ({
         // add settings/options here
-        apiKey: '5b3ce3597851110001cf6248083c3751f2544eb0982346d2c7f6536c'
+        apiKey: '5b3ce3597851110001cf6248083c3751f2544eb0982346d2c7f6536c',
+        drawButtonContent: '',
+    drawButtonStyleClass: 'fa fa-pencil-alt fa-2x',
+    deleteButtonContent: '',
+    deleteButtonStyleClass: 'fa fa-trash fa-2x',
+    distanceButtonContent: '',
+    distanceButtonStyleClass: 'fa fa-road fa-2x',
+    timeButtonContent: '',
+    timeButtonStyleClass: 'fa fa-clock fa-2x',
+    travelModeButton1Content: '',
+    travelModeButton1StyleClass: 'fa fa-car fa-2x',
+    travelModeButton2Content: '',
+    travelModeButton2StyleClass: 'fa fa-bicycle fa-2x',
+    travelModeButton3Content: '',
+    travelModeButton3StyleClass: 'fa fa-male fa-2x',
+    travelModeButton4Content: '',
+    travelModeButton4StyleClass: 'fa fa-wheelchair fa-2x'
     }).addTo(map);
