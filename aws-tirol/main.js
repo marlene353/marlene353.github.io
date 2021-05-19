f5363682b672fd69fac4a1e0ed8c5f8ed83882c1
Marlene Benzinger <marlene.benzinger@student.uibk.ac.at>
2021-05-19 15:32:57 +0200

    fullscreen + minimap

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -2,6 +2,7 @@ let basemapGray = L.tileLayer.provider('BasemapAT.grau');
 
 //https://leafletjs.com/reference-1.7.1.html#map-example
 let map = L.map("map", {
+    fullscreenControl: true,
     center: [47, 11],
     zoom: 9,
     layers: [
@@ -10,7 +11,7 @@ let map = L.map("map", {
 });
 
 let overlays = {
-    staions:L.featureGroup(), 
+    staions: L.featureGroup(),
     temperature: L.featureGroup(),
     snowheight: L.featureGroup(),
     windspeed: L.featureGroup(),
@@ -41,17 +42,17 @@ let awsUrl = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
 fetch(awsUrl)
     .then(response => response.json())
     .then(json => {
-            console.log('Daten konvertiert: ', json);
-            //Stationen
-            for (station of json.features) {
-                // console.log('Station: ', station);
-                //https://leafletjs.com/reference-1.7.1.html#marker
-                let marker = L.marker([
-                    station.geometry.coordinates[1],
-                    station.geometry.coordinates[0]
-                ]);
-                let formattedDate = new Date(station.properties.date);
-                marker.bindPopup(`
+        console.log('Daten konvertiert: ', json);
+        //Stationen
+        for (station of json.features) {
+            // console.log('Station: ', station);
+            //https://leafletjs.com/reference-1.7.1.html#marker
+            let marker = L.marker([
+                station.geometry.coordinates[1],
+                station.geometry.coordinates[0]
+            ]);
+            let formattedDate = new Date(station.properties.date);
+            marker.bindPopup(`
             <h3>${station.properties.name}</h3>
             <ul>
               <li>Datum: ${formattedDate.toLocaleString("de")}</li>
@@ -63,82 +64,90 @@ fetch(awsUrl)
             </ul>
             <a target="_blank" href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.plot}.png">Grafik</a>
             `);
-                marker.addTo(awsLayer);
-                if (station.properties.HS) {
-                    let highlightClass = '';
-                    if (station.properties.HS > 100) {
-                        highlightClass = 'snow-100';
-                    }
-                    if (station.properties.HS > 200) {
-                        highlightClass = 'snow-200';
-                    }
-                    let snowIcon = L.divIcon({
-                        //https://leafletjs.com/reference-1.7.1.html#divicon
-                        html: `<div class="snow-label ${highlightClass}">${station.properties.HS}</div>`
-                    })
-                    let snowMarker = L.marker([
-                        station.geometry.coordinates[1],
-                        station.geometry.coordinates[0]
-                    ], {
-                        icon: snowIcon
-                    });
-                    snowMarker.addTo(overlays.snowheight);
+            marker.addTo(awsLayer);
+            if (station.properties.HS) {
+                let highlightClass = '';
+                if (station.properties.HS > 100) {
+                    highlightClass = 'snow-100';
                 }
-                if (station.properties.WG) {
-                    let windHighlightClass = '';
-                    if (station.properties.WG > 10) {
-                        windHighlightClass = 'wind-10';
-                    }
-                    if (station.properties.WG > 20) {
-                        windHighlightClass = 'wind-20';
-                    }
-                    let windIcon = L.divIcon({
-                        html: `<div class="wind-label ${windHighlightClass}">${station.properties.WG}</div>`,
-                    });
-                    let windMarker = L.marker([
-                        station.geometry.coordinates[1],
-                        station.geometry.coordinates[0]
-                    ], {
-                        icon: windIcon
-                    });
-                    windMarker.addTo(overlays,windspeed);
+                if (station.properties.HS > 200) {
+                    highlightClass = 'snow-200';
                 }
+                let snowIcon = L.divIcon({
+                    //https://leafletjs.com/reference-1.7.1.html#divicon
+                    html: `<div class="snow-label ${highlightClass}">${station.properties.HS}</div>`
+                })
+                let snowMarker = L.marker([
+                    station.geometry.coordinates[1],
+                    station.geometry.coordinates[0]
+                ], {
+                    icon: snowIcon
+                });
+                snowMarker.addTo(overlays.snowheight);
+            }
+            if (station.properties.WG) {
+                let windHighlightClass = '';
+                if (station.properties.WG > 10) {
+                    windHighlightClass = 'wind-10';
+                }
+                if (station.properties.WG > 20) {
+                    windHighlightClass = 'wind-20';
+                }
+                let windIcon = L.divIcon({
+                    html: `<div class="wind-label ${windHighlightClass}">${station.properties.WG}</div>`,
+                });
+                let windMarker = L.marker([
+                    station.geometry.coordinates[1],
+                    station.geometry.coordinates[0]
+                ], {
+                    icon: windIcon
+                });
+                windMarker.addTo(overlays, windspeed);
+            }
 
-                if (station.properties.LT) {
-                    let tempHighlightClass = '';
-                    if (station.properties.LT > 0) {
-                        tempHighlightClass = 'temp-pos';
-                    }
-                    if (station.properties.LT < 0) {
-                        tempHighlightClass = 'temp-neg';
-                    }
-                    let tempIcon = L.divIcon({
-                        html: `<div class="temp-label ${tempHighlightClass}">${station.properties.LT}</div>`,
-                    });
-                    let tempMarker = L.marker([
-                        station.geometry.coordinates[1],
-                        station.geometry.coordinates[0]
-                    ], {
-                        icon: tempIcon
-                    });
-                    tempMarker.addTo(overlays.temperature);
+            if (station.properties.LT) {
+                let tempHighlightClass = '';
+                if (station.properties.LT > 0) {
+                    tempHighlightClass = 'temp-pos';
                 }
-                if (station.properties.LT == 0) {
-                    let tempHighlightClass = 'temp-0';
-                    let tempIcon = L.divIcon({
-                        html: `<div class="temp-label ${tempHighlightClass}">${station.properties.LT}</div>`,
-                    });
-                    let tempMarker = L.marker([
-                        station.geometry.coordinates[1],
-                        station.geometry.coordinates[0]
-                    ], {
-                        icon: tempIcon
-                    });
-                    tempMarker.addTo(tempLayer);
+                if (station.properties.LT < 0) {
+                    tempHighlightClass = 'temp-neg';
                 }
-            } 
+                let tempIcon = L.divIcon({
+                    html: `<div class="temp-label ${tempHighlightClass}">${station.properties.LT}</div>`,
+                });
+                let tempMarker = L.marker([
+                    station.geometry.coordinates[1],
+                    station.geometry.coordinates[0]
+                ], {
+                    icon: tempIcon
+                });
+                tempMarker.addTo(overlays.temperature);
+            }
+            if (station.properties.LT == 0) {
+                let tempHighlightClass = 'temp-0';
+                let tempIcon = L.divIcon({
+                    html: `<div class="temp-label ${tempHighlightClass}">${station.properties.LT}</div>`,
+                });
+                let tempMarker = L.marker([
+                    station.geometry.coordinates[1],
+                    station.geometry.coordinates[0]
+                ], {
+                    icon: tempIcon
+                });
+                tempMarker.addTo(tempLayer);
+            }
+        }
 
 
-                // set map view to all stations
-                map.fitBounds(overlays.stations.getBounds());
-            });
+        // set map view to all stations
+        map.fitBounds(overlays.stations.getBounds());
+    });
+
+//miniMap + Minimize button + minimize
+var miniMap = new L.Control.MiniMap(
+    L.tileLayer.provider("BasemapAT.basemap"), {
+        toggleDisplay: true,
+        minimized: true
+    }
+).addTo(map);
